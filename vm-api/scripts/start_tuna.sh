e4c8711db5654bc5a95a5c45ae0f0797fc39c632
Qequqeq <130912757+Qequqeq@users.noreply.github.com>
2025-05-20 21:17:10 +0300

    Update path in start_tuna.sh

--- vm-api/scripts/start_tuna.sh
+++ vm-api/scripts/start_tuna.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tuna http 8080 --subdomain=alp-virtualka --log /root/database/tuna_logs --rate-limit=2 --location=ru
+tuna http 8080 --subdomain=<yourdomain> --log /root/VirtualizationServer/database/tuna_logs --rate-limit=2 --location=ru
