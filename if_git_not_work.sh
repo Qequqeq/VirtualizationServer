a480146b2617f45ead4fd08e2773cc9c00174484
Qequqeq <130912757+Qequqeq@users.noreply.github.com>
2025-05-22 20:58:42 +0300

    Delete if_git_not_work.sh

--- if_git_not_work.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -L -b /tmp/cookies.txt \
-"https://drive.usercontent.google.com/download?id=1Mjlt5c9IfywGFYvySt7aGffxG3s33rQF&export=download&confirm=t&uuid=6f0cd54f-f138-4cf6-93d3-1ed2c20aa1fc" \
--o alpine_base.qcow2
-mv alpine_base.qcow2 image/
